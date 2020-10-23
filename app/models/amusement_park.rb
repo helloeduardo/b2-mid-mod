fec2011914c949189e8d120d2edd0f8fc59ed6f1
Eduardo Parra <helloeduardo@icloud.com>
2020-10-23 10:27:15 -0600

    Add average ride thrill

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,7 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  def average_ride_thrill
+    rides.average(:thrill)
+  end
 end
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe AmusementPark, type: :model do
   describe 'relationships' do
     it { should have_many :rides }
   end
+
+  describe 'instance methods' do
+    before (:each) do
+      @ap1 = AmusementPark.create!(name: "Hershey Park", price: "$50.00")
+      @ride1 = Ride.create!(name: "Lightning Racer", thrill: 8, amusement_park: @ap1)
+      @ride2 = Ride.create!(name: "Storm Runner", thrill: 9, amusement_park: @ap1)
+      @ride3 = Ride.create!(name: "The Great Bear", thrill: 10, amusement_park: @ap1)
+    end
+
+    it '.average_ride_thrill' do
+      expect(@ap1.average_ride_thrill).to eq(9)
+    end
+  end
 end
