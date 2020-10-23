8f4ae4b56d3ba702e0facd67f089fb08a4a727fa
Eduardo Parra <helloeduardo@icloud.com>
2020-10-23 09:41:43 -0600

    Add relationship tests

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :amusement_park }
+    it { should have_many :mechanic_rides }
+    it { should have_many(:mechanics).through(:mechanic_rides) }
+  end
 end
